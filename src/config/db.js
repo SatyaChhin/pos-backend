8eb2424af70d37972c946dd390a1787d4c446b8b
chhinsatya <97458238+SatyaChhin@users.noreply.github.com>
2024-08-25 23:03:33 +0700

    apply env in projects

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,14 @@
+// Load environment variables from .env file
+require('dotenv').config(); 
+
 const mysql = require("mysql2/promise");
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root", 
-  password: "11112222",
-  database: "pos-system",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   namedPlaceholders: true,
 });
+
 module.exports = { db };
